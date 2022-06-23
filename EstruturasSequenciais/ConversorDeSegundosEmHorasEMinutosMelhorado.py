95acb6a10fe6c9181984e531d241e10cd41603ba
Eduardo Bandeira <106497109+eduhejband@users.noreply.github.com>
2022-06-23 14:35:32 -0300

    Update ConversorDeSegundosEmHorasEMinutosMelhorado.py
    Resolvendo alguns bugs e erros.

--- EstruturasSequenciais/ConversorDeSegundosEmHorasEMinutosMelhorado.py
+++ EstruturasSequenciais/ConversorDeSegundosEmHorasEMinutosMelhorado.py
@@ -3,38 +3,37 @@ def DefinaNome():
     while not Flag:
         try:
             x = int(input("Digite a quantidade de segundos\n"))
+            trn = x
+
+
+            y = (x/(60*60))
+
+            yy = round(y, 0)
+
+            z = y - int(y)
+
+            zz = (6000*(z))/100
+
+            zzz = zz - int(zz)
+            
+            zzzz= (6000*(zzz))/100
+
+            if(x<3600):
+                i = 0
+                print ("Valor em  {} horas  {} minutos e {} segundos ".format(i, int(round(zz,0)),int(round(zzzz,0))))
+            elif (zz >59):
+                i = 0
+                print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)),i,int(round(zzzz,0))))
+            elif (zzzz >59):
+                i = 0
+                print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)), int(round(zz,0)),i))
+            else:
+                print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)), int(round(zz,0)),int(round(zzzz,0))))
             Flag = True
+        
+        
         except(ValueError):
             print("\n\n\nDigite apenas números\n\n\n\n\n\n")
+            exit()
             
-            
-
-trn = DefinaNome()
-
-
-y = (trn/(60*60))
-
-yy = round(y, 0)
-
-z = y - int(y)
-
-
-zz = (6000*(z))/100
-
-
-zzz = zz - int(zz)
-zzzz= (6000*(zzz))/100
-
-
-if(trn<3600):
-    i = 0
-    print ("Valor em  {} horas  {} minutos e {} segundos ".format(i, int(round(zz,0)),int(round(zzzz,0))))
-elif (zz >59):
-    i = 0
-    print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)),i,int(round(zzzz,0))))
-elif (zzzz >59):
-    i = 0
-    print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)), int(round(zz,0)),i))
-
-else:
-    print ("Valor em  {} horas  {} minutos e {} segundos ".format(int(round(y,0)), int(round(zz,0)),int(round(zzzz,0))))
+DefinaNome()
