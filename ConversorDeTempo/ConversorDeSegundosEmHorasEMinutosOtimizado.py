3ecc3acc2d4f1917c462426ca737c6ab7715dbd9
Eduardo Hejazi Bandeira <106497109+eduhejband@users.noreply.github.com>
2022-06-29 15:56:45 -0300

    Create ConversorDeSegundosEmHorasEMinutosOtimizado.py

---
+++ ConversorDeTempo/ConversorDeSegundosEmHorasEMinutosOtimizado.py
@@ -0,0 +1,33 @@
+from math import floor
+
+def DefinaDia():
+    Flag = False
+    while not Flag:
+        try:
+        
+            segundosT = int(input("Digite a quantidade de segundos\n"))
+  
+ 
+            
+            horas = int(segundosT/3600)
+            segundosRest1 = segundosT%3600
+            
+            minutos = int(segundosRest1/60)
+            segundos = segundosRest1%60
+            segundos = int(segundos)
+            
+            print ("Valor em  {} horas  {} minutos e {} segundos ".format(horas,minutos,segundos))
+            Flag = True
+           
+            
+            
+            print("____________________________________________________")
+            print ("\n\n\n\n\n")
+            return DefinaDia()
+        
+        
+        except(ValueError):
+            print("\n\n\nDigite apenas números\n\n\n\n\n\n")
+            exit()
+            
+DefinaDia()
